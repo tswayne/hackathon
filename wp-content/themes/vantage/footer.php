bbefea22202912a2128db9c599fc6f46a9baa0a6
Candi Ligutan <cligutan@gmail.com>
2013-10-03 20:57:14 -0400

    change bg color for listing. added footer ad.

--- wp-content/themes/vantage/footer.php
+++ wp-content/themes/vantage/footer.php
@@ -3,6 +3,10 @@
 
 <?php wp_footer(); ?>
 
+<footer>
+	<img src="http://placehold.it/360x40" alt=""/>
+</footer>
+
 <script src="../wp-content/themes/vantage/js/bootstrap.min.js"></script>
 
 </body>
